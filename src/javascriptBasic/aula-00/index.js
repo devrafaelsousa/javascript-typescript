4e48990bf84a0f7c72b8b674e0758f9d8c6e4da9
devrafaelsousa <rafael.sousa.pereira@mail.usf.edu.br>
2020-08-10 23:46:13 -0300

    first commit

---
+++ src/javascriptBasic/aula-00/index.js
@@ -0,0 +1,16 @@
+/*
+// Exemplos de String's
+console.log('Olá Mundo!!!');
+console.log("Olá Mundo!!!");
+console.log(`Olá Mundo!!!`);
+console.log("Olá 'Mundo!!!'");
+console.log('"Olá" Mundo!!!');
+console.log(`'"Olá" Mundo!!!'`);
+
+// Exemplos de Number's e String's
+console.log(12, 1.25, 'Rafa');
+*/
+
+
+// Exercicio da aula - 00
+console.log('Meu nome é "RAFAEL". Estou aprendendo e reforçando os conceitos de JavaScript às', 11, 'da noite.');
