2bced0ee81ef65ab5964b835e9a39214fb684ce5
devrafaelsousa <rafael.sousa.pereira@mail.usf.edu.br>
2020-11-08 03:10:10 -0300

    "Update names folders"

---
+++ JSArrays/aula-58/index.js
@@ -0,0 +1,32 @@
+
+const nome = 'Rafael, Sousa, Pereira';
+const nomes = nome.split(', ');
+console.log(nomes);
+
+
+
+
+
+
+
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(0, 3);
+
+// console.log(novo);
+
+
+// nomes.push('João');
+// console.log(nomes.length);
+
+// const novo = [...nomes];
+
+// const novo = nomes;
+
+// novo.pop();
+// console.log(nomes);
+// console.log(novo);
+
+//const nomes = new Array('Eduardo', 'Maria', 'Joana');
+// nomes[2] = 'João';
+// delete nomes[2];
+// console.log(nomes);
